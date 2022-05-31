c4efe4f3ea208c9aa550d22f33a7e83e0f93e2df
fabriciojacob <fabricio.jacob@outlook.com>
2022-05-30 22:09:58 -0300

    add

--- src/main/java/com/github/fabriciolfj/orderservice/web/OrderController.java
+++ src/main/java/com/github/fabriciolfj/orderservice/web/OrderController.java
@@ -3,6 +3,8 @@ package com.github.fabriciolfj.orderservice.web;
 import com.github.fabriciolfj.orderservice.domain.Order;
 import com.github.fabriciolfj.orderservice.domain.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +18,13 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     private final OrderService orderService;
 
     @GetMapping
     public Flux<Order> getAllOrders(@AuthenticationPrincipal Jwt jwt) {
+        log.info("Find all orders to {}", jwt.getSubject());
         return orderService.getAllOrders(jwt.getSubject());
     }
 
